af15938ede846f13cd3240ae202a19e6b34ff63a
Luis Duran <neoluis87@gmail.com>
2017-09-20 12:49:28 -0300

    Primer commit de la solucion

---
+++ SeminarioWars/IArma.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SeminarioWars
+{
+    interface IArma
+    {
+        int poderPara(Soldado soldado);
+    }
+}
---
+++ SeminarioWars/Soldado.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeminarioWars
+{
+    class Soldado
+    {
+        string nombre;
+        int vida;
+        List<IArma> armas; 
+
+        public int poderTotal()
+        {
+            return armas.Sum(cadaArma => cadaArma.poderPara(this));
+        }
+
+
+    }
+}
